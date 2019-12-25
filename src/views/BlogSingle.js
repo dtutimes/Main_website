ab6586d14ab580cce66f37ca396ea3c6a4a07cbf
Manish Sahani <friday@Manishs-MacBook-Pro.local>
2019-12-25 23:10:04 +0530

    scroll added

--- src/views/BlogSingle.js
+++ src/views/BlogSingle.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 import Single from "./posts-sections/BlogSingle";
+
+
 const BlogSingle = () => {
-    return ( 
-        <Single />
-     );
-}
- 
+  return (
+    <>
+      <Single />
+    </>
+  );
+};
+
 export default BlogSingle;
