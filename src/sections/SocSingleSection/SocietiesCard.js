23f1429516b34ae5b2012e60f6bfe83314d00783
Neha <goyalneha2607@gmail.com>
2021-03-25 15:25:49 +0530

    card changes

--- src/sections/SocSingleSection/SocietiesCard.js
+++ src/sections/SocSingleSection/SocietiesCard.js
@@ -6,6 +6,32 @@ import {
 
 const SocietiesCard = props => {
   return (
+    // <div>
+    //   <div class="t-item">
+    //     <div class="t-image">
+    //       <img src="http://placehold.it/360x440" alt="Team Member" class="img-responsive"/>
+    //       <div class="t-description">
+    //         <div class="content-wrapper">
+    //           <h4 class="h-alt">Hello!</h4>
+    //           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //     <div class="t-info">
+    //       <h4 class="t-name">Philip Lee</h4>
+    //       <span class="t-role">CEO &amp; Founder</span>
+    //       <ul class="social-links">
+    //         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+    //         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+    //         <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+    //         <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+    //       </ul>
+    //     </div>  
+    //   </div>
+    // </div>
+
+
     <Card
       data-background="image"
       style={{
@@ -24,18 +50,26 @@ const SocietiesCard = props => {
         <p style={{ textOverflow: "ellipsis" }}>{props.des.slice(0,400)}...</p>
         {/* {console.log(props.des.slice(0,100))} */}
         <CardFooter>
+          
+
+          <div class="t-info">
           <Row>
-            <Col md="4">
-              <p>Head-Incharge:{props.head}</p>
+            <Col md="6">
+              <h4 class="t-name">{props.head}</h4>
+              <span class="t-role">Head-Incharge</span>
             </Col>
-            <Col md="4"></Col>
-            <Col md="4">
-              <p>Pr-Incharge:{props.prHead}</p>
+            <Col md="6">
+              <h4 class="t-name">{props.prHead}</h4>
+              <span class="t-role">PR-Incharge</span>
             </Col>
           </Row>
+          </div>
         </CardFooter>
+        
       </CardBody>
     </Card>
+
+
     // <div className="card transition">
     //   <h2 className="transition h2_soc">{props.name}<br /><small className="small_soc">{props.des} </small></h2>
     //   <div className="cta-container transition"></div>
