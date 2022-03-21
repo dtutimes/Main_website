a857ddf7a113344dab0f9442182ee41fefff7e60
Angad Sethi <angadsethi_2k18co066@dtu.ac.in>
2022-03-21 17:00:06 +0530

    Update TeamCard.js

--- src/sections/TeamSections/TeamCard.js
+++ src/sections/TeamSections/TeamCard.js
@@ -13,7 +13,7 @@ const TeamCard = props => {
     <>
       <Card className="card-profile card-plain">
         <CardImg top tag="div">
-          <img alt={name} className="img" src={img} style="object-fit: cover;"/>
+          <img alt={name} className="img" src={img} style=""/>
         </CardImg>
         <CardBody>
           <CardTitle tag="h4">{name}</CardTitle>
